189f2b354bc31f8aace1373347c3c0d906f1ecb8
MoamenWalid <momenelnager@gmail.com>
2026-06-10 19:29:24 +0300

    update

--- dist/oop.js
+++ dist/oop.js
@@ -1,18 +1,37 @@
 "use strict";
+/**
+ * Notes:
+ * Access Modifiers: (public, private)
+ */
 /* OOP - Class & Object */
 class Account {
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
+        Account._numOfAccounts++;
+    }
+    static get numOfAccounts() {
+        return Account._numOfAccounts;
     }
     deposet(amount) {
-        this.balance = this.balance + amount;
+        if (amount <= 0)
+            throw Error("Amount can't be less than one");
+        this._balance = this._balance + amount;
+    }
+    get balance() {
+        return this._balance;
+    }
+    get username() {
+        return this._username;
+    }
+    set username(value) {
+        this._username = value;
     }
 }
-let moamenAccount = new Account(1, "Moamen Walid", 10);
-let mazenAccount = new Account(2, "Mazen Walid", 20);
-console.log(moamenAccount);
-moamenAccount.deposet(20);
-console.log(moamenAccount);
+Account._numOfAccounts = 0;
+let acc1 = new Account(1, "Moamen Walid", 10);
+let acc2 = new Account(2, "Mazen Walid", 20);
+let acc3 = new Account(2, "Mazen Walid", 20);
+console.log(Account.numOfAccounts);
 /* OOP - Class & Object */ 
--- src/oop.ts
+++ src/oop.ts
@@ -1,20 +1,48 @@
+/**
+ * Notes:
+ * Access Modifiers: (public, private)
+ */
+
 /* OOP - Class & Object */
 class Account {
   id: number;
   owner: string;
-  balance: number;
+  private _balance: number;
+  private _username?: string;
+  private static _numOfAccounts: number = 0;
   
   constructor(id: number, owner: string, balance: number) {
     this.id = id;
     this.owner = owner;
-    this.balance = balance;
+    this._balance = balance;
+    Account._numOfAccounts++;
   }
 
-  deposet(amount: number) {
-    this.balance = this.balance + amount;
+  static get numOfAccounts(): number {
+    return Account._numOfAccounts;
+  }
+
+  deposet(amount: number): void {
+    if (amount <= 0) throw Error("Amount can't be less than one");
+    this._balance = this._balance + amount;
+  }
+
+  get balance(): number {
+    return this._balance;
+  }
+
+  get username(): string | undefined {
+    return this._username;
+  }
+
+  set username(value: string) {
+    this._username = value;
   }
 }
 
-let moamenAccount = new Account(1, "Moamen Walid", 10);
-let mazenAccount = new Account(2, "Mazen Walid", 20);
+let acc1 = new Account(1, "Moamen Walid", 10);
+let acc2 = new Account(2, "Mazen Walid", 20);
+let acc3 = new Account(2, "Mazen Walid", 20);
+
+console.log(Account.numOfAccounts);
 /* OOP - Class & Object */
